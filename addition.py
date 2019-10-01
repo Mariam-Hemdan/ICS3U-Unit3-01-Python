ed6a91b6eedaa322f8286de407ffa146cba18df8
Ubuntu <ubuntu@ip-172-31-24-146.ec2.internal>
2019-10-01 15:58:55 +0000

    send file to git hub

---
+++ addition.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Mariam Hemdan
+# Created on: Sep 2019
+# This program calculates total addition numbers
+
+
+def main():
+    # this function calculates addition two numbers
+
+    # input
+    first_number = int(input("Enter the first number: "))
+    second_number = int(input("Enter the second number: "))
+
+    # process
+    sum = first_number + second_number
+
+    # output
+    print("")
+    print("The total is {}".format(sum))
+
+
+if __name__ == "__main__":
+    main()
